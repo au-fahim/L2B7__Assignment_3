6ae710ee26387ab377d05c1563dfa7cda5789411
au-fahim <dev.aufahim@gmail.com>
2026-06-15 15:05:17 +0600

    Added Solutions for SQL Queries 4 - 7

--- QUERY.sql
+++ QUERY.sql
@@ -105,3 +105,59 @@ FROM
     Bookings
 WHERE 
     payment_status IS NULL;
+
+
+-- =========================================================================
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+-- =========================================================================
+SELECT 
+    b.booking_id, 
+    u.full_name, 
+    m.fixture, 
+    b.total_cost
+FROM 
+    Bookings b
+INNER JOIN 
+    Users u ON b.user_id = u.user_id
+INNER JOIN 
+    Matches m ON b.match_id = m.match_id;
+
+
+-- ==========================================================================
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+-- ==========================================================================
+SELECT 
+    u.user_id, 
+    u.full_name, 
+    b.booking_id
+FROM 
+    Users u
+LEFT JOIN 
+    Bookings b ON u.user_id = b.user_id;
+
+
+-- =========================================================================
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+-- =========================================================================
+SELECT 
+    booking_id, 
+    match_id, 
+    total_cost
+FROM 
+    Bookings
+WHERE 
+    total_cost > (SELECT AVG(total_cost) FROM Bookings);
+
+
+-- =========================================================================
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+-- =========================================================================
+SELECT 
+    match_id, 
+    fixture, 
+    base_ticket_price
+FROM 
+    Matches
+ORDER BY 
+    base_ticket_price DESC
+LIMIT 2 OFFSET 1;
